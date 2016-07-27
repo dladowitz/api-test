1065adc57809cfb52cd2e2c704b05060cb6ef83c
David Ladowitz <david@ladowitz.com>
2016-07-26 23:09:03 -0700

    testing out Sabisu_rails

--- Gemfile
+++ Gemfile
@@ -28,6 +28,12 @@ gem 'jquery-rails'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'sabisu_rails', github: "IcaliaLabs/sabisu-rails"
+gem 'compass-rails', '~> 2.0.2'
+gem 'furatto'
+gem 'font-awesome-rails'
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,7 @@
-# require 'api_constraints'
-#
-# Rails.application.routes.draw do
-#   devise_for :users
-#   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-#     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-#       resources :users, :only => [:show]
-#     end
-#   end
-# end
-
-# require 'api_constraints'
-#
-# Rails.application.routes.draw do
-#   devise_for :users
-#   # Api definition
-#   namespace :api, defaults: { format: :json },
-#                               constraints: { subdomain: 'api' }, path: '/'  do
-#     scope module: :v1,
-#               constraints: ApiConstraints.new(version: 1, default: true) do
-#       # We are going to list our resources here
-#       resources :users, :only => [:show]
-#     end
-#   end
-# end
-
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },

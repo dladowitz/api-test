bcd311892eeaa18708d11043cdc1d65981413dc7
David Ladowitz <david@ladowitz.com>
2016-07-26 21:43:38 -0700

    movin to rails_helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,46 @@
+# require 'api_constraints'
+#
+# Rails.application.routes.draw do
+#   devise_for :users
+#   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+#     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+#       resources :users, :only => [:show]
+#     end
+#   end
+# end
+
+# require 'api_constraints'
+#
+# Rails.application.routes.draw do
+#   devise_for :users
+#   # Api definition
+#   namespace :api, defaults: { format: :json },
+#                               constraints: { subdomain: 'api' }, path: '/'  do
+#     scope module: :v1,
+#               constraints: ApiConstraints.new(version: 1, default: true) do
+#       # We are going to list our resources here
+#       resources :users, :only => [:show]
+#     end
+#   end
+# end
+
 require 'api_constraints'
 
 Rails.application.routes.draw do
   devise_for :users
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show]
     end
   end
 end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe User do
   before { @user = FactoryGirl.build(:user) }
@@ -10,4 +10,9 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
 end
